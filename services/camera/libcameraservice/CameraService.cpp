3f8261b5a3534d7bc022c3040346c2a4c6669984
Akihiro Maeda <sola.1980.a@gmail.com>
2014-02-01 16:06:10 +0900

    Support for JCROM

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -48,6 +48,10 @@
 #include "utils/CameraTraces.h"
 #include "CameraDeviceFactory.h"
 
+#define MY_CAMERA_SOUND_DIR "/data/theme/sounds/camera/"
+#define MY_CAMERA_SOUND_SHUTTER "/data/theme/sounds/camera/camera_click.ogg"
+#define MY_CAMERA_SOUND_RECORD "/data/theme/sounds/camera/VideoRecord.ogg"
+
 namespace android {
 
 // ----------------------------------------------------------------------------
@@ -911,8 +915,21 @@ void CameraService::loadSound() {
     int disableSound = atoi(value);
 
     if(!disableSound) {
-        mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
-        mSoundPlayer[SOUND_RECORDING] = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
+        // JCROM
+        char property[PROPERTY_VALUE_MAX];
+        if (property_get("persist.sys.force.hobby", property, NULL) > 0) {
+            if (strcmp(property, "true") == 0) {
+                // WARNING this method "newMediaPlayer" doesn't return verify value...
+                mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer(MY_CAMERA_SOUND_SHUTTER);
+                mSoundPlayer[SOUND_RECORDING] = newMediaPlayer(MY_CAMERA_SOUND_RECORD);            
+            } else {
+                mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
+                mSoundPlayer[SOUND_RECORDING] = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
+            }
+        } else {
+            mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
+            mSoundPlayer[SOUND_RECORDING] = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
+        }
     }
     else {
         mSoundPlayer[SOUND_SHUTTER] = NULL;
